e228936a85cc07242f88ab67f610ea35f7ab9206
Aaron Marth <aaron_marth32@outlook.com>
2025-09-12 22:08:49 -0400

    initial commit - header and introduction

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Introduction } from "./introduction/introduction";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Introduction],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
